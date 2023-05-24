d1f72ca112f2ef1d57c9d7b1610d9e41c9716d2c
Jay <japusen32@gmail.com>
2023-05-24 00:42:53 -0700

    First steps for API Access Token

---
+++ app/src/main/java/com/example/lurkforreddit/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.lurkforreddit
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lurkforreddit.network.TokenApi
+import com.example.lurkforreddit.ui.theme.LurkForRedditTheme
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            LurkForRedditTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "CoroutineCreationDuringComposition")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Scaffold() {
+        val scope = rememberCoroutineScope()
+        scope.launch {
+            TokenApi.post()
+        }
+    }
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LurkForRedditTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/lurkforreddit/model/AccessToken.kt
@@ -0,0 +1,15 @@
+package com.example.lurkforreddit.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AccessToken(
+    @SerialName("access_token")
+    val accessToken: String,
+    @SerialName("token_type")
+    val tokenType: String,
+    @SerialName("expires_in")
+    val expiresIn: Long,
+    val scope: String
+)
