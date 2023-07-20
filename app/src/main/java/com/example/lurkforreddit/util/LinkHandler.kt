31193a072e012889dc6160058b34a9079551a6fc
Jay <japusen32@gmail.com>
2023-07-20 16:30:04 -0700

    function to handle navigation for links depending on domain

--- app/src/main/java/com/example/lurkforreddit/util/LinkHandler.kt
+++ app/src/main/java/com/example/lurkforreddit/util/LinkHandler.kt
@@ -1,2 +1,27 @@
 package com.example.lurkforreddit.util
 
+import android.content.Context
+import android.net.Uri
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.navigation.NavController
+import java.net.URI
+
+fun openPostLink(
+    context: Context,
+    navController: NavController,
+    url: String
+) {
+    val uri = URI(url)
+    val host = uri.host
+
+    if (host.equals("i.redd.it"))
+        navController.navigate("image/${encodedUrl(url)}")
+    else if (host.equals("v.redd.it"))
+        navController.navigate("video/${encodedUrl(url)}")
+    else {
+        val intent = CustomTabsIntent
+            .Builder()
+            .build()
+        intent.launchUrl(context, Uri.parse(url))
+    }
+}
