297762dac79f30fef35d32b2e304aa4f400812eb
Jay <japusen32@gmail.com>
2023-08-02 23:31:47 -0700

    zoom and pan for images

--- app/src/main/java/com/example/lurkforreddit/ui/components/ImageLink.kt
+++ app/src/main/java/com/example/lurkforreddit/ui/components/ImageLink.kt
@@ -1,14 +1,25 @@
 package com.example.lurkforreddit.ui.components
 
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.IntSize
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 
@@ -17,6 +28,10 @@ fun ImageLink(
     url: String,
     modifier: Modifier = Modifier
 ) {
+    var scale by remember { mutableStateOf(1f) }
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+    var size by remember { mutableStateOf(IntSize.Zero) }
     val context = LocalContext.current
     Scaffold(
         containerColor = Color.Black
@@ -25,16 +40,36 @@ fun ImageLink(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(paddingValues)
-        )
-        AsyncImage(
-            model = ImageRequest.Builder(context)
-                .data(url)
-                .crossfade(true)
-                .build(),
-            placeholder = null,
-            contentDescription = null,
-            contentScale = ContentScale.Fit,
-            modifier = Modifier.fillMaxSize()
-        )
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(context)
+                    .data(url)
+                    .crossfade(true)
+                    .build(),
+                placeholder = null,
+                contentDescription = null,
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .onSizeChanged { size = it }
+                    .pointerInput(Unit) {
+                        detectTransformGestures { _, pan, zoom, _ ->
+                            scale = maxOf(1f, minOf(scale * zoom, 5f))
+                            val maxX = (size.width * (scale - 1)) / 2
+                            val minX = -maxX
+                            offsetX = maxOf(minX, minOf(maxX, offsetX + pan.x))
+                            val maxY = (size.height * (scale - 1)) / 2
+                            val minY = -maxY
+                            offsetY = maxOf(minY, minOf(maxY, offsetY + pan.y))
+                        }
+                    }
+                    .graphicsLayer(
+                        scaleX = scale,
+                        scaleY = scale,
+                        translationX = offsetX,
+                        translationY = offsetY
+                    )
+            )
+        }
     }
 }
