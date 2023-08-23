861a4bcbb1052874ef3214db9cf40c3db0a44e43
Jay <japusen32@gmail.com>
2023-08-23 13:26:58 -0700

    cleaned imports

--- app/src/main/java/com/example/lurkforreddit/MainActivity.kt
+++ app/src/main/java/com/example/lurkforreddit/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.lurkforreddit.ui.LurkApp
-import com.example.lurkforreddit.ui.theme.LurkForRedditTheme
+import com.example.lurkforreddit.theme.LurkForRedditTheme
 
 
 class MainActivity : ComponentActivity() {
